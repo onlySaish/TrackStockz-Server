769ef6387b74cfdb44d16ad1d97617f53c0af174
Saish More <onlysainok24@gmail.com>
2026-01-13 21:16:39 +0530

    Final Fixes.

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send("Trackstockz Server is Live !!");
+});
 // Import Routers
 import userRouter from "./routes/user.router.js"
 import customerRouter from "./routes/customer.router.js"
